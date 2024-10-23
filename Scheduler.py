4cf030e5f7aa01fb715af07952029513d9f1e113
Krzysztof Szewczyk <krzysztof5138@gmail.com>
2024-10-23 13:57:38 +0200

    Add first version of try_assign_crew

--- Scheduler.py
+++ Scheduler.py
@@ -41,19 +41,19 @@ class Flight:
         If it's not possible it returns false, with data about failure
         """
         self.aircraft = aircraft
-        unavilib = self.data.aircraft_unavailabilities
-        unavilib = unavilib[unavilib["Registration"] == self.aircraft]
+        unavailab = self.data.aircraft_unavailabilities
+        unavailab = unavailab[unavailab["registration"] == self.aircraft]
 
         failures = dict()
 
-        for i in range(len(unavilib)):
-            start_unavilib = read_time(unavilib.loc[i,"start"]) 
-            finish_unavilib = read_time(unavilib.loc[i,"finish"])
+        for i in range(len(unavailab)):
+            start_unavailab = read_time(unavailab.loc[i,"start"]) 
+            finish_unavailab = read_time(unavailab.loc[i,"finish"])
 
-            if read_time(self.start_time) <= start_unavilib <= read_time(self.end_time):
-                failures["start_time_of_unavilib_within_flight_time"] = [read_time(self.start_time), start_unavilib, read_time(self.end_time)]
-            if read_time(self.start_time) <= finish_unavilib <= read_time(self.end_time):
-                failures["finish_time_of_unavilib_within_flight_time"] = [read_time(self.start_time), finish_unavilib, read_time(self.end_time)]
+            if read_time(self.start_time) <= start_unavailab <= read_time(self.end_time):
+                failures["start_time_of_unavailab_within_flight_time"] = [read_time(self.start_time), start_unavailab, read_time(self.end_time)]
+            if read_time(self.start_time) <= finish_unavailab <= read_time(self.end_time):
+                failures["finish_time_of_unavailab_within_flight_time"] = [read_time(self.start_time), finish_unavailab, read_time(self.end_time)]
         
         restricts = self.data.aircraft_restrictions
         if self.aircraft in restricts[restricts["airport"] == self.end]["disallowed_model"].values:
@@ -77,8 +77,40 @@ class Flight:
         """assigns crew, if it's possible, and then it returns (true, None).
         If it's not possible it returns false, with data about failure
         """
+
+        aircraft_data = self.data.aircraft[self.data.aircraft['registration'] == self.aircraft]
+        failures = dict()
+        ranks = []
+
         self.crew = crew_list
-        #TODO : check if crew has qualifications 
+        for crew_member in self.crew:
+            unavailab = self.data.crew_unavailabilities
+            unavailab = unavailab[unavailab["crew_id"] == crew_member]
+
+            for i in range(len(unavailab)):
+                start_unavailab = read_time(unavailab.loc[i,"start"]) 
+                finish_unavailab = read_time(unavailab.loc[i,"finish"])
+
+                if read_time(self.start_time) <= start_unavailab <= read_time(self.end_time):
+                    failures["start_time_of_unavailab_within_flight_time"] = [read_time(self.start_time), start_unavailab, read_time(self.end_time)]
+                if read_time(self.start_time) <= finish_unavailab <= read_time(self.end_time):
+                    failures["finish_time_of_unavailab_within_flight_time"] = [read_time(self.start_time), finish_unavailab, read_time(self.end_time)]
+            
+            crew_member_data = self.data.crew[self.data.crew['crew_id'] == crew_member]
+            
+            if crew_member_data['specs'] != aircraft_data['family']:
+                failures['crew_member_has_invalid_qualifications_for_aircraft'] = [crew_member, crew_member_data['specs'], aircraft_data['family']]
+
+            ranks.append(crew_member_data['rank'])
+        
+        
+        if len(failures) == 0:
+            return tuple(True, None)
+        else:
+            return tuple(False, failures)
+
+
+        #TODO : check if number of crew members is appropriate 
     
     def reset_aircraft(self):
         self.aircraft = None
